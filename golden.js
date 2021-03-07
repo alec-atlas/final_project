fcdb9c61c5bfd48f8197b4a61e932a494dff0ad3
Tony Zhu <tony.zhu@kellogg.northwestern.edu>
2021-03-07 11:32:53 -0600

    Integrate get_drivers lambda function

--- golden.js
+++ golden.js
@@ -1,39 +1,47 @@
-firebase.auth().onAuthStateChanged(async function(user) {
+firebase.auth().onAuthStateChanged(async function (user) {
   if (user) {
-    // Signed in
-    console.log('signed in')
-    let db = firebase.firestore()
+    // console.log('signed in')
 
-    let querySnapshot = await db.collection('drivers').get()
-
-    let drivers = querySnapshot.docs
-
-    for (let i=0; i<drivers.length; i++) {
-      let driverID = drivers[i].id
-      let driverName = drivers[i].name
-      let driverFee = drivers[i].fee
-      let driverProfilePic = drivers[i].profilepic
-
-      document.querySelector('.drivers').insertAdjacentHTML('beforeend', `
-      <div class=".driver-${driverID} p-4 w-full md:w-1/2 lg:w-1/3">
-        <div class="border h-full p-4 flex flex-col">
-            <h2 class="text-lg font-bold mb-4">${driverName}</h2>
-            <div class="mb-4"><img src="${driverProfilePic}">
-          </div>
-            <div class="mb-4 text-gray-900">
-              I am the best driver in all of Chicago!
-            </div>
-          <div class="mt-auto text-yellow-500 text-2xl">$${driverFee}/hour</div>
-        </div>
-      </div>
-      `)
+    // fetch all driver data from the get_drivers endpoint and render driver profiles
+    let response = await fetch('/.netlify/functions/get_drivers')
+    let drivers = await response.json()
+    for (let i = 0; i < drivers.length; i++) {
+      let driver = drivers[i]
+      renderDriver(driver)
     }
+
+    // let db = firebase.firestore()
+
+    // let querySnapshot = await db.collection('drivers').get()
+
+    // let drivers = querySnapshot.docs
+
+    // for (let i=0; i<drivers.length; i++) {
+    //   let driverID = drivers[i].id
+    //   let driverName = drivers[i].name
+    //   let driverFee = drivers[i].fee
+    //   let driverProfilePic = drivers[i].profilepic
+
+    //   document.querySelector('.drivers').insertAdjacentHTML('beforeend', `
+    //   <div class=".driver-${driverID} p-4 w-full md:w-1/2 lg:w-1/3">
+    //     <div class="border h-full p-4 flex flex-col">
+    //         <h2 class="text-lg font-bold mb-4">${driverName}</h2>
+    //         <div class="mb-4"><img src="${driverProfilePic}">
+    //       </div>
+    //         <div class="mb-4 text-gray-900">
+    //           I am the best driver in all of Chicago!
+    //         </div>
+    //       <div class="mt-auto text-yellow-500 text-2xl">$${driverFee}/hour</div>
+    //     </div>
+    //   </div>
+    //   `)
+    // }
 
     document.querySelector('.sign-in-or-sign-out').innerHTML = `
       <button class="text-pink-500 underline sign-out">Sign Out</button>
     `
 
-    document.querySelector('.sign-out').addEventListener('click', function(event) {
+    document.querySelector('.sign-out').addEventListener('click', function (event) {
       console.log('sign out clicked')
       firebase.auth().signOut()
       document.location.href = 'index.html'
@@ -41,7 +49,7 @@ firebase.auth().onAuthStateChanged(async function(user) {
 
 
 
-// WHAT SIGNED OUT USERS SEE 
+    // WHAT SIGNED OUT USERS SEE 
   } else {
     // Signed out
     console.log('signed out')
@@ -61,3 +69,20 @@ firebase.auth().onAuthStateChanged(async function(user) {
     ui.start('.sign-in-or-sign-out', authUIConfig)
   }
 })
+
+async function renderDriver(driverData) {
+  document.querySelector('.drivers').insertAdjacentHTML('beforeend', `
+    <div class=".driver-${driverData.id} p-4 w-full md:w-1/2 lg:w-1/3">
+      <div class="border h-full p-4 flex flex-col">
+          <h2 class="text-lg font-bold mb-2">${driverData.name}</h2>
+          <h1 class="italic mb-4">${driverData.email}</h1>
+          <div class="mb-4"><img src="${driverData.profilePicUrl}">
+        </div>
+          <div class="mb-4 text-gray-900">
+            I am the best driver in all of Chicago!
+          </div>
+        <div class="mt-auto text-yellow-500 text-2xl">$${driverData.fee}/hour</div>
+      </div>
+    </div>
+  `)
+}
