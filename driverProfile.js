d0b1b96ac161877cee426a270649d0c6b3263875
Tony Zhu <tony.zhu@kellogg.northwestern.edu>
2021-03-16 19:10:21 -0500

    fixed picture issue

--- driverProfile.js
+++ driverProfile.js
@@ -76,7 +76,7 @@ async function renderDriverProfile(driver) {
         <div class="mt-8 mb-8 md:flex md:flex-wrap">
             <div class="border md:p-4 w-full">
                 <div class="md:p-4 md:flex">
-                    <div class="md:w-1/2 p-2"><img src="${driver.profilepic}"></div>
+                    <div class="md:w-1/2 p-2"><img src="${driver.profilePicUrl}"></div>
                     <div class="md:w-1/2 p-2">
                         <div class="text-2xl font-bold text-gray-900">Driver Info:</div>
                             <ul class="list-disc ml-6">
