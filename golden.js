74d84f54d9f718c64748785a03770b0d94730b05
Tony Zhu <tony.zhu@kellogg.northwestern.edu>
2021-03-16 20:21:30 -0500

    driver address privacy

--- golden.js
+++ golden.js
@@ -13,7 +13,7 @@ async function renderDriver(driver) {
     <div class=".driver-${driver.id} p-4 md:w-1/3">
       <div class="border-2 border-yellow-500 md:h-104 p-4 flex flex-col bg-gray-50">
           <h2 class="text-lg font-bold mb-2">${driver.name}</h2>
-          <h1 class="italic mb-4">${driver.address}, ${driver.city} ${driver.zip}</h1>
+          <h1 class="italic mb-4">${driver.city}, ${driver.state}</h1>
           <div class="mb-4"><img class="object-contain h-64 w-full object-center" src="${driver.profilePicUrl}">
         </div>
         <div class="mt-auto text-yellow-500 text-2xl font-bold">$${driver.fee}/hour</div>
