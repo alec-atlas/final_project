d64e43308a8874b0922f72a13a6096470ed04bf0
Alec-Atlas <alec.atlas@kellogg.northwestern.edu>
2021-03-28 18:34:41 -0500

    formatting changes

--- dashboard.js
+++ dashboard.js
@@ -45,19 +45,19 @@ firebase.auth().onAuthStateChanged(async function (user) {
 
 async function renderDriver(driver) {
   document.querySelector('.drivers').insertAdjacentHTML('beforeend', `
-    <div class=".driver-${driver.id} p-4 md:w-1/3">
-      <div class="border-2 border-yellow-500 md:h-104 p-4 flex flex-col bg-gray-50">
+    <div class=".driver-${driver.id} p-4 md:w-1/3 w-full h-full">
+      <div class="border-2 border-yellow-500 p-4 flex flex-col bg-gray-50">
         <h2 class="text-lg font-bold mb-2">${driver.name}</h2>
         <h1 class="italic mb-4">${driver.city}, ${driver.state}</h1>
         <div class="mb-4"><img class="object-contain h-64 w-full object-center" src="${driver.profilePicUrl}"></div>
-        <div class="mb-4 text-gray-900">About Me: ${driver.bio}</div>
+        <div class="mb-4 text-gray-900">${driver.bio}</div>
         <div class="mt-auto text-yellow-500 text-2xl font-bold">$${driver.fee}/hour</div>
-        <a href="driverProfile.html?driverId=${driver.id}">
-          <button
-            class="block text-white bg-yellow-500 rounded w-full my-4 py-2 font-bold text-xl uppercase mx-auto container">
-            <span>learn more about me!</span>
-          </button>
-        </a>
+          <a href="driverProfile.html?driverId=${driver.id}">
+            <button
+              class="block text-white bg-yellow-500 rounded w-full my-4 py-2 font-bold text-xl uppercase mx-auto container">
+              <span>learn more about me!</span>
+            </button>
+          </a>
       </div>
     </div>
   `)
